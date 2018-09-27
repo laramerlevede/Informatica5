43529886df6927e167a41b12dbf9dcd83c3777e4
laramerlevede <43027936+laramerlevede@users.noreply.github.com>
2018-09-27 09:39:21 +0200

    oppervlakte cirkel opgelost

---
+++ 04 - Variabelen/test.py
@@ -0,0 +1,12 @@
+# coëfficiënten van x^2, x^1 en x^o
+a = float(input('Geef a: '))
+b = float(input('Geef b: '))
+c = float(input('Geef c: '))
+
+print('a =', a, 'b =', b, 'c =', c)
+
+# discriminant berekenen
+d = (b * b) - (4 * a * c)
+
+#discriminant tonen
+print(d)
