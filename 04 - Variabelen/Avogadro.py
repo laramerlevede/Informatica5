4d13ddd78d627a2608f5741a607f9d343df8346b
laramerlevede <43027936+laramerlevede@users.noreply.github.com>
2018-09-30 19:00:42 +0200

    avogadro opgelost

---
+++ 04 - Variabelen/Avogadro.py
@@ -0,0 +1,17 @@
+NA = (6.020 * 10 ** 23)
+M = 32.06
+
+n = float(input('Geef aantal deeltjes S: '))
+
+N = n * NA
+resultaat1 = str(N)
+
+m = M * n
+resultaat2 = str(m)
+
+print(resultaat2)
+print(resultaat1)
+
+
+
+
