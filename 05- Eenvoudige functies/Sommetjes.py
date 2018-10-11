9e770db100c5d011ebc92ac2efb8b042afecc16d
laramerlevede <43027936+laramerlevede@users.noreply.github.com>
2018-10-11 08:30:37 +0200

    sommetjes opgelost

---
+++ 05- Eenvoudige functies/Sommetjes.py
@@ -0,0 +1,32 @@
+#invoer
+a = float(input('a: '))
+b = float(input('b: '))
+from random import random, randint, seed
+
+#berekeningen
+getal_1 = (10 ** 0) * a
+getal_2 = (10 ** 0) * b
+getal_3 = getal_1 + getal_2
+
+getal_4 = (10 ** 1) * a
+getal_5 = (10 ** 1) * b
+getal_6 = getal_4 + getal_5
+
+getal_7 = (10 ** 2) * a
+getal_8 = (10 ** 2) * b
+getal_9 = getal_7 + getal_8
+
+getal_10 = (10 ** 3) * a
+getal_11 = (10 ** 3) * b
+getal_12 = getal_10 + getal_11
+
+getal_13 = (10 ** 4) * a
+getal_14 = (10 ** 4) * b
+getal_15 = getal_13 + getal_14
+
+#uitvoer
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_1, getal_2, getal_3))
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_4, getal_5, getal_6))
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_7, getal_8, getal_9))
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_10, getal_11, getal_12))
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_13, getal_14, getal_15))
