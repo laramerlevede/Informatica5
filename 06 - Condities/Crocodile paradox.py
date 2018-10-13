183c8b5c1df8c3278859ec2b03619b2b64620286
laramerlevede <43027936+laramerlevede@users.noreply.github.com>
2018-10-13 11:24:36 +0200

    crocodile paradox opgelost

--- 06 - Condities/Crocodile paradox.py
+++ 06 - Condities/Crocodile paradox.py
@@ -1,2 +1,10 @@
 #antwoord op de vraag
 wie_zenuwen = input('Wie heeft er stalen zenuwen? ')
+
+#wat gebeurt er met het kind?
+if wie_zenuwen == 'vader':
+    print('Krokodil geeft kind terug')
+elif wie_zenuwen == 'krokodil':
+    print('Krokodil eet kind op')
+else:
+    print('Moe van het denken')
